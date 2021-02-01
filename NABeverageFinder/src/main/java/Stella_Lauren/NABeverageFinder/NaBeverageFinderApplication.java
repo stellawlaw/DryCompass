862ca2437671878190541c078ea53ca5cb72122f
stella.w.law@gmail.com <stella.w.law@gmail.com>
2021-02-01 16:47:04 -0500

    adding backend api

---
+++ NABeverageFinder/src/main/java/Stella_Lauren/NABeverageFinder/NaBeverageFinderApplication.java
@@ -0,0 +1,13 @@
+package Stella_Lauren.NABeverageFinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaBeverageFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaBeverageFinderApplication.class, args);
+	}
+
+}
